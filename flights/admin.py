9e691f9de897e7db16cfe03f311bc265d2837cf3
yashdoshi247 <yash.career99@gmail.com>
2023-03-15 15:50:16 +0400

    Added users app

--- flights/admin.py
+++ flights/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Flights)
+
+#Adding custom admin configurations
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id","origin","destination","duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("p_flights",)
+
+admin.site.register(Flights, FlightAdmin)
 admin.site.register(Airports)
-admin.site.register(Passengers)
+admin.site.register(Passengers, PassengerAdmin)
